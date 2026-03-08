0949ec9995b42f4555580ba0e4bc18cd73793c25
FloppaMakerStart <pashapon76@gmail.com>
2026-03-08 22:59:13 +0300

    Added Battle

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/FloppaMakerStart/UltraHubOfficial/refs/heads/main/obfuscated.lua.txt
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FloppaMakerStart/UltraHubOfficial/refs/heads/main/obfuscated.lua.txt"))()
